a80617533f6837fcbe86e0a88931d647b4c12d71
AnibalOS <theanibalos@gmail.com>
2025-12-05 01:06:52 +0000

    Update greeting text in index.ts

--- apps/web-blog/src/index.ts
+++ apps/web-blog/src/index.ts
@@ -5,7 +5,7 @@ const rootEl = document.querySelector("#root");
 if (rootEl) {
 	rootEl.innerHTML = `
     <h1 class="text-3xl font-bold underline">
-      Hola Mundo Caheado y Separado HOLA
+      Hola Mundo Caheado y Separado HOLA 2
     </h1>
 `;
 }
